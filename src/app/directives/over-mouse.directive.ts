9a2de9a68ba352ad11b72f306a066f1d342906ad
Lizvan27 <lizvanmandy@gmail.com>
2023-01-06 14:56:40 -0500

    desarrollo directivas

--- src/app/directives/over-mouse.directive.ts
+++ src/app/directives/over-mouse.directive.ts
@@ -1,15 +1,15 @@
-import { Directive,ElementRef,Input,OnInit} from '@angular/core';
+import { Directive,ElementRef,Input,OnChanges} from '@angular/core';
 
 @Directive({
   selector: '[overUp]' //el nombre es personalizado
 })
-export class OverMouseDirective implements OnInit{
+export class OverMouseDirective implements OnChanges{
   @Input() invalid!: boolean | undefined;
   $element = this.elementRef.nativeElement as HTMLInputElement
 
   constructor(private elementRef: ElementRef) {
   }
-  ngOnInit(): void {
+  ngOnChanges(): void {
     if(this.invalid){
       this.$element.classList.add('border-red-500')
     } else {
